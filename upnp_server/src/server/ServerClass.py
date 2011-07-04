013807e34bdf976d43484595dabe4943eadc5259
pszafer <pszafer@gmail.com>
2011-07-05 01:22:51 +0200

    maybe now

---
+++ upnp_server/src/server/ServerClass.py
@@ -0,0 +1,37 @@
+'''
+Created on 05-07-2011
+
+@author: Pawel Szafer, pszafer@gmail.com
+'''
+
+from twisted.internet import reactor
+from coherence.base import Coherence 
+
+
+class ServerClass(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.start()
+        
+    def check_device(self, device):
+        print "check device"
+        print "found device %s of type %s - %r" %(device.get_friendly_name(),
+                                              device.get_device_type(),
+                                              device.client)
+                
+    def start(self):
+        print "I'm started"
+        config = {'logmode':'warning'}
+        c = Coherence(config)
+        c.connect(self.check_device, 'detection completed')
+        
+print "start"
+reactor.callWhenRunning(ServerClass)
+reactor.run()
+print "stop"
